aa487be4769f0a11e1790bc2804cbc2aeae59f11
Thibault Guilhem <thibault.guilhem@gmail.com>
2026-06-21 22:57:26 +0200

    configure MongoDB connection and environment variables

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
+require("dotenv").config();
+
 const http = require("http");
+const mongoose = require("mongoose");
 const app = require("./app");
 
 const port = process.env.PORT || 4000;
@@ -7,6 +10,15 @@ app.set("port", port);
 
 const server = http.createServer(app);
 
-server.listen(port, () => {
-  console.log(`Server listening on http://localhost:${port}`);
-});
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+
+    server.listen(port, () => {
+      console.log(`Server listening on http://localhost:${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error", error.message);
+  });
