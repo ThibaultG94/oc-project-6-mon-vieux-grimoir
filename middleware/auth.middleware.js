183096e77bf20475a6d0d8eb68d8d24ae1662534
Thibault Guilhem <thibault.guilhem@gmail.com>
2026-06-23 13:21:38 +0200

    add JWT authentication middleware

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      throw new Error("Token manquant ou format invalide");
+    }
+
+    const token = authHeader.split(" ")[1];
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    const userId = decodedToken.userId;
+
+    req.auth = { userId };
+
+    next();
+  } catch (error) {
+    res.status(401).json({ error });
+  }
+};
